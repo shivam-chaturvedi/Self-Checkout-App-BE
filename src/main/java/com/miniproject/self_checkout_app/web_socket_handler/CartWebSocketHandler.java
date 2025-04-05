daf29aa88e7b8368969b6cd22fad73bec5f25fc0
shivam chaturvedi <chaturvedishivam598@gmail.com>
2025-04-05 18:43:44 +0530

    websocket payments qr display on tft

--- src/main/java/com/miniproject/self_checkout_app/web_socket_handler/CartWebSocketHandler.java
+++ src/main/java/com/miniproject/self_checkout_app/web_socket_handler/CartWebSocketHandler.java
@@ -1,8 +1,13 @@
 package com.miniproject.self_checkout_app.web_socket_handler;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
+import org.antlr.v4.runtime.misc.TestRig;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.BinaryMessage;
 import org.springframework.web.socket.CloseStatus;
@@ -20,87 +25,157 @@ import com.miniproject.self_checkout_app.service.UserCartService;
 import com.miniproject.self_checkout_app.utils.QRCodeDecoder;
 
 //ADDING T TO START OF THE MESSAGE WILL DISPLAY MESSAGE IN GREEN IN ESP32 AND ADDING F WILL DISPLAY IN RED AS A ERROR
-
+// SENDING SHOW_PAYMENT_QR WILL DISPLAY PAYMENT QR CODE IN TFT DISPLAY 
 @Component
 public class CartWebSocketHandler extends AbstractWebSocketHandler {
-    private final StoreCartService storeCartService;
-    private final UserCartService userCartService;
-    private final ProductService productService;
-    private Long storeCartId = null;
+	private final StoreCartService storeCartService;
+	private final UserCartService userCartService;
+	private final ProductService productService;
+	private Long storeCartId = null;
+	private boolean isPaymentQrCodeShowing = false;
+	private final Map<Long, List<WebSocketSession>> CART_SESSIONS = new HashMap<Long, List<WebSocketSession>>();
 
-    public CartWebSocketHandler(StoreCartService storeCartService, UserCartService userCartService,
-                                 ProductService productService) {
-        this.storeCartService = storeCartService;
-        this.userCartService = userCartService;
-        this.productService = productService;
-    }
+	public CartWebSocketHandler(StoreCartService storeCartService, UserCartService userCartService,
+			ProductService productService) {
+		this.storeCartService = storeCartService;
+		this.userCartService = userCartService;
+		this.productService = productService;
+	}
 
-    @Override 
-    public void afterConnectionEstablished(WebSocketSession session) throws IOException {
-        String query = session.getUri().getQuery();
-        if (query != null && query.startsWith("cartId=")) {
-            try {
-                this.storeCartId = Long.parseLong(query.split("=")[1]);
-                Optional<StoreCart> storeCart = storeCartService.getStoreCartById(storeCartId);
-                if (storeCart.isEmpty()) {
-                    session.sendMessage(new TextMessage("FInvalid cart ID: " + storeCartId));
-                    session.close();
-                }
-                User attachedUser= storeCart.get().getCurrentUser();
-                if(attachedUser!=null) {
-                	session.sendMessage(new TextMessage("TConnected:-"+attachedUser.getEmail()));
-                }
-            } catch (NumberFormatException e) {
-                session.sendMessage(new TextMessage("FInvalid cart ID format."));
-                session.close();
-            }
-        } else {
-            session.sendMessage(new TextMessage("FMissing or invalid cart ID."));
-            session.close();
-        }
-    }
-    
+	public void sendTextMessageToAllSessions(String message) {
+		for (WebSocketSession session : CART_SESSIONS.getOrDefault(storeCartId, new ArrayList<WebSocketSession>())) {
+			if (session.isOpen()) {
+				try {
+					session.sendMessage(new TextMessage(message));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
-    @Override
-    protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws IOException {
-        try {
-        	System.out.println(message);
-            String decodedData = QRCodeDecoder.decodeWithZbarImg(message.getPayload());
-            if (decodedData != null) {
-                processDecodedData(session, decodedData);
-            } else {
-                session.sendMessage(new TextMessage("TScan To Add Product!"));
-            }
-        } catch (Exception e) {
-            session.sendMessage(new TextMessage("FError: " + e.getMessage()));
-        }
-    }
-   
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws IOException {
+		String query = session.getUri().getQuery();
+		if (query != null && query.startsWith("cartId=")) {
+			try {
+				if (query.contains("&")) {
+//					for non main nodemcu
+					this.storeCartId = Long.parseLong(query.split("&")[0].split("=")[1]);
+				} else {
+					this.storeCartId = Long.parseLong(query.split("=")[1]);
+				}
+				Optional<StoreCart> storeCart = storeCartService.getStoreCartById(storeCartId);
+				if (storeCart.isEmpty()) {
+					session.sendMessage(new TextMessage("FInvalid cart ID: " + storeCartId));
+					session.close();
+				}
+				User attachedUser = storeCart.get().getCurrentUser();
+				if (attachedUser != null) {
+					session.sendMessage(new TextMessage("TConnected:- " + attachedUser.getEmail()));
+					CART_SESSIONS.putIfAbsent(storeCartId, new ArrayList<WebSocketSession>());
+					CART_SESSIONS.get(storeCartId).add(session);
 
-    
-    private void processDecodedData(WebSocketSession session, String decodedData) throws Exception {
-        Optional<StoreCart> storeCart = storeCartService.getStoreCartById(storeCartId);
-        if (storeCart.isPresent()) {
-            Long  userCartId = storeCartService.getActiveUserCartId(storeCart.get());
-            
-            if (userCartId == null) {
-                session.sendMessage(new TextMessage("FNo active user cart found. Please attach a cart."));
-                return;
-            }
-            Optional<Product> product = productService.getProduct(Long.parseLong(decodedData));
-            if (product.isPresent()) {
-                CartItem cartItem = userCartService.addNewItemToUserCart(userCartId, product.get());
-                session.sendMessage(new TextMessage("TAdded: " + product.get().getName() + " x " + cartItem.getQuantity()));
-            } else {
-                session.sendMessage(new TextMessage("FProduct not found for scanned code."));
-            }
-        } else {
-            session.sendMessage(new TextMessage("FInvalid store cart."));
-        }
-    }
+				}
+			} catch (NumberFormatException e) {
+				session.sendMessage(new TextMessage("FInvalid cart ID format."));
+				session.close();
+			}
+		} else {
+			session.sendMessage(new TextMessage("FMissing or invalid cart ID."));
+			session.close();
+		}
+	}
 
-    @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
-        System.out.println("Connection closed: " + session.getId());
-    }
+	@Override
+	protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) {
+		try {
+			String decodedData = QRCodeDecoder.decodeWithZbarImg(message.getPayload());
+			if (decodedData != null) {
+				processDecodedData(session, decodedData);
+			} else {
+				if (!isPaymentQrCodeShowing) {
+					session.sendMessage(new TextMessage("TScan To Add Product!"));
+				}
+			}
+		} catch (Exception e) {
+			try {
+				if (!isPaymentQrCodeShowing) {
+					session.sendMessage(new TextMessage("FError: " + e.getMessage()));
+				}
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+	} 
+
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) {
+		try {
+
+			String payload = message.getPayload();
+
+			if (payload.startsWith("SHOW_PAYMENT_QR")) {
+//				this will show payment qr in tft
+				if (!isPaymentQrCodeShowing) {
+					sendTextMessageToAllSessions(payload);
+					isPaymentQrCodeShowing = true;
+				}
+			}
+
+			else {
+				isPaymentQrCodeShowing = false;
+
+				Optional<StoreCart> storeCart = storeCartService.getStoreCartById(storeCartId);
+				if (storeCart.isPresent()) {
+					Long userCartId = storeCartService.getActiveUserCartId(storeCart.get());
+
+					if (userCartId == null) {
+						sendTextMessageToAllSessions("FNo active user cart found. Please attach a cart.");
+						return;
+					}
+					Optional<Product> product = productService.getProductByRfidTag(payload.trim());
+					if (product.isPresent()) {
+						CartItem cartItem = userCartService.addNewItemToUserCart(userCartId, product.get());
+						sendTextMessageToAllSessions(
+								"TAdded: " + product.get().getName() + " x " + cartItem.getQuantity());
+					} else {
+						sendTextMessageToAllSessions("FProduct not found for scanned code.");
+					}
+				} else {
+					session.sendMessage(new TextMessage("FInvalid store cart."));
+				}
+
+			}
+		} catch (Exception e) {
+			sendTextMessageToAllSessions("FError: " + e.getMessage());
+		}
+	}
+
+	private void processDecodedData(WebSocketSession session, String decodedData) throws Exception {
+		Optional<StoreCart> storeCart = storeCartService.getStoreCartById(storeCartId);
+		if (storeCart.isPresent()) {
+			Long userCartId = storeCartService.getActiveUserCartId(storeCart.get());
+
+			if (userCartId == null) {
+				sendTextMessageToAllSessions("FNo active user cart found. Please attach a cart.");
+				return;
+			}
+			Optional<Product> product = productService.getProduct(Long.parseLong(decodedData));
+			if (product.isPresent()) {
+				CartItem cartItem = userCartService.addNewItemToUserCart(userCartId, product.get());
+				sendTextMessageToAllSessions("TAdded: " + product.get().getName() + " x " + cartItem.getQuantity());
+			} else {
+				sendTextMessageToAllSessions("FProduct not found for scanned code.");
+			}
+		} else {
+			session.sendMessage(new TextMessage("FInvalid store cart."));
+		}
+	}
+
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+		System.out.println("Connection closed: " + session.getId());
+		CART_SESSIONS.getOrDefault(storeCartId, new ArrayList<WebSocketSession>()).remove(session);
+	}
 }
